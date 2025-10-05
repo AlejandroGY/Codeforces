3abc986e00a58877713ad0c0523bea51440b9d4b
AlejandroGY <alejandro.gy96@gmail.com>
2025-10-05 13:25:47 -0600

    fix directories

---
+++ Practice/313B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+int main( ) {
+   std::ios_base::sync_with_stdio(0);
+   std::cin.tie(0);
+   std::cout.tie(0);
+
+   std::string s;
+   std::cin >> s;
+
+   int a[s.size( ) + 1] = { };
+   for (int i = 1; i < s.size( ); ++i) {
+      a[i] += (s[i] == s[i - 1]) + a[i - 1];
+   }
+
+   int q;
+   std::cin >> q;
+   for (int i = 0; i < q; ++i) {
+      int ini, fin;
+      std::cin >> ini >> fin;
+      std::cout << a[fin - 1] - a[ini - 1] << "\n";
+   }
+}
